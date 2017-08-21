c3327b76290b6730f9d6785ddb769243e81c03ea
fukuroda <fukuroder@live.jp>
2017-08-22 02:05:32 +0900

    Add files via upload

---
+++ walsh_generator.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+import numpy as np
+import scipy.io.wavfile
+import os
+from itertools import chain
+
+def walsh_transform(x):
+    def flatten(l):
+        return list(chain.from_iterable(l))
+
+    n = len(x)
+    if(n == 1):
+        return x
+    else:
+        # Radix-2
+        x0 = walsh_transform( [(a+b) for a, b in zip(x, x[n/2:])] )
+        x1 = walsh_transform( [(a-b) for a, b,in zip(x, x[n/2:])] )
+        return flatten(zip(x0, x1))
+
+if __name__ == '__main__':
+    resolution = 2**16
+    max_n = 2**10
+    samplerate = 44100
+    gain = (2**14)
+
+    os.mkdir("wav")
+
+    for n in xrange(max_n):
+        # translate to gray code
+        g = n ^ (n>>1)
+
+        # walsh spectrum
+        sequency = np.zeros((resolution,))
+        sequency[g] = 1
+
+        # inverse transform
+        write_frames = np.array( walsh_transform(sequency) )*gain
+
+        # write
+        scipy.io.wavfile.write(
+                'wav/walsh_%04d_%d.wav' % (n,resolution),
+                samplerate,
+                write_frames.astype(np.int16))
+
